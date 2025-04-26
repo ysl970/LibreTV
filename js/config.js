873d2c193234dc3546b676e796aa127791c6f667
Haitai Hu <hityhoo@gmail.com>
2025-04-26 14:06:20 +0800

    Revert "Update config.js"
    This reverts commit fc86c177b9917232eddd14a9bfbdbbfdc8ce15a7.

--- js/config.js
+++ js/config.js
@@ -12,8 +12,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: 'Iatiah TV',
+    url: 'https://tv.huhaitai.com',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
     version: '1.0.3'
