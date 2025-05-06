1963ec34de9677c4aa7136121cd09d093ba2ab5c
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-05-06 22:40:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -34,14 +34,17 @@ const API_SITES = {
     tyyszy: { api: 'https://tyyszy.com', name: '天涯资源' },
     zy360: { api: 'https://360zy.com', name: '360资源' },
     wolong: { api: 'https://wolongzyw.com', name: '卧龙资源' },
-    hwba: { api: 'https://cjwba.com', name: '华为吧资源' },
+    hwba: { api: 'https://cjhwba.com', name: '华为吧资源' },
     jisu: { api: 'https://jszyapi.com', name: '极速资源', detail: 'https://jszyapi.com' },
     dbzy: { api: 'https://dbzy.com', name: '豆瓣资源' },
     bfzy: { api: 'https://bfzyapi.com', name: '暴风资源' },
     mozhua: { api: 'https://mozhuazy.com', name: '魔爪资源' },
     mdzy: { api: 'https://www.mdzyapi.com', name: '魔都资源' },
     ruyi: { api: 'https://cj.rycjapi.com', name: '如意资源' },
-    zuid: { api: 'https://api.zuidapi.com', name: '最大资源' }
+    zuid: { api: 'https://api.zuidapi.com', name: '最大资源' },
+    yinghua: { api: 'https://m3u8.apiyhzy.com', name: '樱花资源' },
+    baidu: { api: 'https://api.apibdzy.com', name: '百度云资源' },
+    wujin: { api: 'https://api.wujinapi.me', name: '无尽资源' }
 };
 
 // 聚合搜索配置
