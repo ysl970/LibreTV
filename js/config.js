9e8f7168155c3e37ce06e11d487b43625e62f8bd
jdsai0 <109773735+jdsai0@users.noreply.github.com>
2025-05-08 21:50:08 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,8 +12,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: '奇异TV',
+    url: 'https://www.baidu.com/',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
     version: '1.0.3'
