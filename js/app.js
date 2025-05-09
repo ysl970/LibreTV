712cc517f7a757f00ad0e51b4921d99a59ad28a0
jdsai0 <109773735+jdsai0@users.noreply.github.com>
2025-05-10 07:15:12 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 全局变量
-let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["tyyszy", "bfzy", "ruyi"]'); // 默认选中天涯资源、暴风资源和如意资源
+let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["tyyszy","dyttzy", "bfzy", "ruyi"]'); // 默认选中天涯资源、暴风资源和如意资源
 let customAPIs = JSON.parse(localStorage.getItem('customAPIs') || '[]'); // 存储自定义API列表
 
 // 添加当前播放的集数索引
@@ -12,7 +12,7 @@ let currentVideoTitle = '';
 let episodesReversed = false;
 
 // 页面初始化
-document。addEventListener('DOMContentLoaded'， function() {
+document.addEventListener('DOMContentLoaded', function() {
     // 初始化API复选框
     initAPICheckboxes();
     
@@ -28,7 +28,7 @@ document。addEventListener('DOMContentLoaded'， function() {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 仅选择天涯资源、暴风资源和如意资源
-        selectedAPIs = ["tyyszy", "bfzy"， "ruyi"];
+        selectedAPIs = ["tyyszy", "bfzy","dyttzy", "ruyi"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
         
         // 默认选中过滤开关
@@ -1124,4 +1124,4 @@ if (config.auth.enabled) {
 }
 
 // 或者针对特定路由
-app.use('/api', authMiddleware);
+app。use('/api', authMiddleware);
