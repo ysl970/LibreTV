ea982684667a77865cc1f13f636167564cbc698a
Binhehe <75666750+Binhehe@users.noreply.github.com>
2025-05-10 20:35:13 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -43,7 +43,7 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
+        adult: false
     },
     zy360: {
         api: 'https://360zy.com',
@@ -85,32 +85,32 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
+        adult: false
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
+        adult: false
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
+        adult: false
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
+        adult: false
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
+        adult: false
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
+        adult: false,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -120,7 +120,7 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
+        adult: false
     },
     yinghua: {
         api: 'https://m3u8.apiyhzy.com',
