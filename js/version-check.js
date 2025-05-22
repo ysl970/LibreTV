fbffc86c68401242ce7508f412486ae6d7049199
MasterChiefCN <49179444+MasterChiefCN@users.noreply.github.com>
2025-05-22 21:30:27 +0800

    Update version-check.js

--- js/version-check.js
+++ js/version-check.js
@@ -148,7 +148,7 @@ function addVersionInfoToFooter() {
                 const updateBtn = versionElement.querySelector('span');
                 if (updateBtn) {
                     updateBtn.addEventListener('click', () => {
-                        window.open('https://github.com/LibreSpark/LibreTV', '_blank');
+                        window.open('https://bowen.games/', '_blank');
                     });
                 }
             }, 100);
