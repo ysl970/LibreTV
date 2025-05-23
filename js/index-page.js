ef78dcfbe1eae6b451b02e80335c485ccfcc2ad0
帅帅的小站 <shuaidaoya@gmail.com>
2025-05-23 22:04:49 +0800

    更新 index-page.js

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - 像素圣殿`, 
                         window.location.href
                     );
                 } catch (e) {
