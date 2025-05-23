054aa3c72a46209ad0333fc17eddc1c2a34653c9
帅帅的小站 <shuaidaoya@gmail.com>
2025-05-23 22:40:44 +0800

    更新 player.js

--- js/player.js
+++ js/player.js
@@ -214,7 +214,7 @@ function initializePageContent() {
     }
 
     // 设置页面标题
-    document.title = currentVideoTitle + ' - LibreTV播放器';
+    document.title = currentVideoTitle + ' - 像素圣殿 播放器';
     document.getElementById('videoTitle').textContent = currentVideoTitle;
 
     // 初始化播放器
