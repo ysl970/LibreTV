e7dd54fe5e5e21797b13ae4c80539f6e31e57ea7
帅帅的小站 <shuaidaoya@gmail.com>
2025-05-23 22:21:49 +0800

    更新 config.js

--- js/config.js
+++ js/config.js
@@ -12,8 +12,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: '像素圣殿',
+    url: 'https://tv.ssdxz.cn',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
