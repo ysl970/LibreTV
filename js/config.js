994b663484afda3d8203edcdf82b1faa02eb3003
驼铃电商社 <93114244+TLS-802@users.noreply.github.com>
2025-05-24 15:06:24 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,8 +12,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: 'YTPPTV',
+    url: 'https://tv.199908.top',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
