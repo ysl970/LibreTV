436c0e159e119e69bc8f3ad48156eb5afdda5641
驼铃电商社 <93114244+TLS-802@users.noreply.github.com>
2025-06-11 22:48:05 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('打开缓存');
         return cache.addAll(urlsToCache);
       })
   );
